86a03d4f14a972ab744bf0a1bdb15ededcc83b8a
Jaydev Desai <jaydevdesai15@gmail.com>
2019-03-25 08:08:23 +0000

    Added notes.php

---
+++ api/notes.php
@@ -0,0 +1,49 @@
+<?php
+	function get_note($request) {
+		if (!(isset($request['post']['tripId']))) {
+			return response(array("message" => "Please pass all parameters"), 400);
+		}
+		$tripId = $request['post']['tripId'];
+		$res = execute_sql('SELECT * FROM user_notes WHERE user_id=?' , $tripId)->get_result();
+		if (!$res) {
+			return response(array(
+				"message" => "Error executing database operation"
+			), 500);
+		}
+		
+		$note = array();
+		while($row = mysqli_fetch_assoc($res)) {
+			unset($row['user_id']);
+			array_push($note, $row);
+		}
+		return response(array(
+			"message" => "Note fetch successfully",
+			"note" => $note
+		));
+	}
+	
+	function update_note($request){
+		if (!(isset($request['post']['tripId']) && isset($request['post']['noteId'])
+			&& isset($request['post']['noteText']))) {
+			return response(array("message" => "Please pass all parameters"), 400);
+		}
+		
+		$tripId = $request['post']['tripId'];
+		$noteId = $request['post']['noteId'];
+		$noteText = $request['post']['noteText'];
+		
+		$res = execute_sql('INSERT INTO user_notes (trip_id, note_text) VALUES (?, ?) ON DUPLICATE KEY UPDATE note_text=?'), $tripId, $noteText, $noteText)
+		
+		if ($res->errno != 0) {
+			return response(array(
+				"message" => "error while executing sql",
+				"description" => $res->error
+			), 500);
+		}
+		
+		return response(array(
+        "message" => "Note Saved."
+		));
+	}
+	
+?>
