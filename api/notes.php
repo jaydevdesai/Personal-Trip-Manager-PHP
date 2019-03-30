a1278cefa3ed324f13bf93d852371ee627a2b5fd
Juned Khatri <juned.khatri31@gmail.com>
2019-03-31 00:31:31 +0530

    Update notes.php syntax errors

--- api/notes.php
+++ api/notes.php
@@ -4,21 +4,22 @@
 			return response(array("message" => "Please pass all parameters"), 400);
 		}
 		$tripId = $request['post']['tripId'];
-		$res = execute_sql('SELECT * FROM user_notes WHERE user_id=?' , $tripId)->get_result();
+		$res = execute_sql('SELECT * FROM user_notes WHERE trip_id=?', $tripId)->get_result();
 		if (!$res) {
 			return response(array(
 				"message" => "Error executing database operation"
 			), 500);
 		}
 		
-		$note = array();
 		while($row = mysqli_fetch_assoc($res)) {
 			unset($row['user_id']);
-			array_push($note, $row);
+			return response(array(
+				"message" => "Note fetch successfully",
+				"note" => $row
+			));
 		}
 		return response(array(
-			"message" => "Note fetch successfully",
-			"note" => $note
+			"message" => "No notes"
 		));
 	}
 	
@@ -32,7 +33,7 @@
 		$noteId = $request['post']['noteId'];
 		$noteText = $request['post']['noteText'];
 		
-		$res = execute_sql('INSERT INTO user_notes (trip_id, note_text) VALUES (?, ?) ON DUPLICATE KEY UPDATE note_text=?'), $tripId, $noteText, $noteText)
+		$res = execute_sql('INSERT INTO user_notes (trip_id, note_text) VALUES (?, ?) ON DUPLICATE KEY UPDATE note_text=?', $tripId, $noteText, $noteText);
 		
 		if ($res->errno != 0) {
 			return response(array(
@@ -42,7 +43,7 @@
 		}
 		
 		return response(array(
-        "message" => "Note Saved."
+        "message" => "Note Saved"
 		));
 	}
 	
